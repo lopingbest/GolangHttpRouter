b0e263065c454e2fadc865f0ce8ca6dacd10780f
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-17 16:56:06 +0700

    feat:router_pattern_test add TestRouterPatternCatchAllParameter

--- router_pattern_test.go
+++ router_pattern_test.go
@@ -29,3 +29,22 @@ func TestRouterPatternNamedParameter(t *testing.T) {
 
 	assert.Equal(t, "Product 1 Item 1", string(body))
 }
+
+func TestRouterPatternCatchAllParameter(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/images/*image", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		image := params.ByName("image")
+		text := "Image : " + image
+		fmt.Fprint(writer, text)
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/images/small/profile.png", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Image : /small/profile.png", string(body))
+}
