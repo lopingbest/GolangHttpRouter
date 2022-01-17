b5b26e319e0c603dc518fbc3cee2c97bedd78e4a
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-17 19:03:51 +0700

    feat:middleware_test add TestMiddleware

---
+++ middleware_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type LogMiddleware struct {
+	http.Handler
+}
+
+func (middleware LogMiddleware) serveHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Receive request")
+	middleware.Handler.ServeHTTP(w, r)
+}
+
+func TestMiddleware(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		fmt.Fprint(writer, "Middleware")
+	})
+
+	middleware := LogMiddleware{router}
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	middleware.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Middleware", string(body))
+}
